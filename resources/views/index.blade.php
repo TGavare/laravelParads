c8670cca8c0c8fdaa9ffe02ef88b75ffb2b00fca
Gustavs Lapiņš <g.lapins@protonmail.com>
2020-08-19 21:34:43 +0300

    Index page layout

---
+++ resources/views/index.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Challenge app</title>
+</head>
+<body>
+    <!-- left section div -->
+    <div class="left-section">
+        <p>Create your own challenge</p>
+        <span><a href="new-challenge"></a></span>
+        <a class="button" href="#">Create your own challenge</a>
+    </div>
+
+    <!-- mid section div -->
+    <div class="">
+        <h1 class="">Most popular</h1>
+        <div>
+            <img src="#" alt="challenge-image.jpg"/>
+        </div>
+    </div>
+
+    <!-- right section div -->
+    <div class="">
+        <select name="Date">
+            <!-- options -->
+        </select>
+        <select name="Categories">
+            <!-- options -->
+        </select>
+        <a href="#">Go</a>
+    </div>
+</body>
+</html>
