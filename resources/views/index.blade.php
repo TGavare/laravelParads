cf91cbe4b7d3754eb917875d3baefc953d7a4e37
Gustavs Lapiņš <g.lapins@protonmail.com>
2020-08-19 23:57:22 +0300

    Modified index page layout

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,29 +7,27 @@
 
 @section('content')
 
-    <!-- left section div -->
-    <div class="left-section">
-        <p>Create your own challenge</p>
-        <span><a href="new-challenge"></a></span>
-        <a class="button" href="#">Create your own challenge</a>
-    </div>
-
-    <!-- mid section div -->
-    <div class="">
-        <h1 class="">Most popular</h1>
-        <div>
-            <img src="#" alt="challenge-image.jpg"/>
+    <div class="row">
+        <div class="col-sm sec">
+            <div class="left-section">
+                <p>Create your own challenge</p>
+                <a href="#"><span class="plus"></span></a>
+            </div>
+        </div>
+        <div class="col-sm sec">
+            <div class="">
+                <select name="Date">
+                    <!-- options -->
+                </select>
+                <select name="Categories">
+                    <!-- options -->
+                </select>
+                <a href="#">Go</a>
+            </div>
         </div>
     </div>
+    <div class="container">
 
-    <!-- right section div -->
-    <div class="">
-        <select name="Date">
-            <!-- options -->
-        </select>
-        <select name="Categories">
-            <!-- options -->
-        </select>
-        <a href="#">Go</a>
     </div>
+
 @endsection
