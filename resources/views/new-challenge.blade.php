0d480c8350cdbb6b975ac8c0fc8eb221f6bcdb88
Gustavs Lapiņš <g.lapins@protonmail.com>
2020-08-20 21:01:50 +0300

    Form input names

--- resources/views/new-challenge.blade.php
+++ resources/views/new-challenge.blade.php
@@ -12,23 +12,23 @@
             <h3>New Challenge</h3>
             <div class="form-group">
                 <label>Title</label>
-                <input class="form-control" type="text">
+                <input class="form-control" type="text" name="title">
             </div>
             <div class="form-group">
                 <label>Description</label>
-                <textarea class="form-control"></textarea>
+                <textarea class="form-control" name="desc"></textarea>
             </div>
             <div class="form-group">
                 <label>Keywords</label>
-                <input class="form-control" type="text">
+                <input class="form-control" type="text" name="keyword">
             </div>
             <div class="form-group">
                 <label>Date</label>
-                <input class="form-control" type="date">
+                <input class="form-control" type="date" name="date">
             </div>
             <div class="form-group">
                 <label>Image</label>
-                <input class="image-input" type="file">
+                <input class="image-input" type="file" name="image">
             </div>
             <button type="submit" class="btn btn-outline-primary my-2 my-sm-0">Submit</button>
         </form>
