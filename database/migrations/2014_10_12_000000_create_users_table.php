4ad3f8883744c17caea47249eff480e7fcc86a25
Gustavs Lapiņš <g.lapins@protonmail.com>
2020-08-30 23:31:03 +0300

    Created user roles

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Schema;
 
 class CreateUsersTable extends Migration
@@ -19,9 +20,21 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->integer('role_id')->unsigned();
             $table->rememberToken();
             $table->timestamps();
         });
+
+        Schema::table('users', function (Blueprint $table) {
+            $table->foreign('role_id')->references('id')->on('roles');
+        });
+
+        DB::table('users')->insert([
+            'name' => "admin",
+            'email' => "root@admin.xyz",
+            'password' => Hash::make("admin"),
+            'role_id' => 2,
+        ]);
     }
 
     /**
